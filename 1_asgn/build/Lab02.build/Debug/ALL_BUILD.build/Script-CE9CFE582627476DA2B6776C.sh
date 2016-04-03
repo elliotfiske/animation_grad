955f11f0b7006600ac555d53de11b2d007a211fd
elliotfiske <elliotfiske@gmail.com>
2016-04-02 21:36:14 -0700

    Finishing up inverse thingy

---
+++ 1_asgn/build/Lab02.build/Debug/ALL_BUILD.build/Script-CE9CFE582627476DA2B6776C.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/elliotfiske/animation_grad/1_asgn/build -f /Users/elliotfiske/animation_grad/1_asgn/build/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
